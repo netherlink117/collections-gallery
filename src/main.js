91d2cef39bc249d8cc7d36f3f861e6693f63bf43
Daniel Gutierrez <netherlink117@outlook.com>
2021-05-02 10:47:21 -0700

    Add font awesome 5

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import axios from "./http";
-
+// import "@fortawesome/fontawesome-free";
 require("@/assets/styles.sass");
 createApp(App).use(axios).use(store).use(router).mount("body");
