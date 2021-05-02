150947e263475a4658f27f69bd394b4bf8117245
Daniel Gutierrez <netherlink117@outlook.com>
2021-05-02 11:03:55 -0700

    Add breadcrumbs to header

--- src/components/HeroHead.vue
+++ src/components/HeroHead.vue
@@ -1,10 +1,35 @@
 <template>
-  <header class="hero-head">
-    <h1 class="title">Collections Gallery</h1>
+  <header class="hero-head breadcrumb is-centered">
+    <ul>
+      <li
+        v-for="breadcrumb of breadcrumbs"
+        v-bind:key="breadcrumb"
+        v-on:click="navigate(breadcrumb)"
+      >
+        <a>{{ breadcrumb.name }}</a>
+      </li>
+    </ul>
   </header>
 </template>
 <script>
 export default {
-  name: "HeroHead"
+  name: "HeroHead",
+  computed: {
+    breadcrumbs() {
+      return this.$store.state.breadcrumbs;
+    }
+  },
+  methods: {
+    navigate(breadcrumb) {
+      const index = this.breadcrumbs.findIndex(
+        (b) => breadcrumb.name === b.name
+      );
+      let breadcrumbs = [];
+      for (let i = 0; i <= index; i++) {
+        breadcrumbs.push(this.breadcrumbs[i]);
+      }
+      this.$store.commit("setBreadcrumbs", breadcrumbs);
+    }
+  }
 };
 </script>
