1a484306510d9502404f683b47f169ec31d028aa
Daniel Gutierrez <netherlink117@outlook.com>
2021-05-23 19:23:46 -0700

    Add localStorage support

--- src/store/index.js
+++ src/store/index.js
@@ -2,58 +2,70 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    view: "grid",
-    order: "asc",
-    separate: true,
-    directories: true,
-    files: true,
-    collections: null,
-    history: [],
-    index: null,
-    report: null
+    server: localStorage.getItem("server") || window.location.origin,
+    view: localStorage.getItem("view") || "grid",
+    order: localStorage.getItem("view") || "asc",
+    separate: JSON.parse(localStorage.getItem("separate") || "true"),
+    directories: JSON.parse(localStorage.getItem("directories") || "true"),
+    files: JSON.parse(localStorage.getItem("files") || "true"),
+    collections: JSON.parse(localStorage.getItem("collections") || "null"),
+    history: JSON.parse(localStorage.getItem("history") || "[]"),
+    index: JSON.parse(localStorage.getItem("index") || "null"),
+    report: JSON.parse(localStorage.getItem("report") || "null")
   },
   mutations: {
     setView(state, payload) {
       state.view = payload;
+      localStorage.setItem("view", state.view);
     },
     setOrder(state, payload) {
       state.order = payload;
+      localStorage.setItem("order", state.order);
     },
     setDirectories(state, payload) {
       state.directories = payload;
+      localStorage.setItem("directories", state.directories);
     },
     setFiles(state, payload) {
       state.files = payload;
+      localStorage.setItem("files", state.directories);
     },
     setCollections(state, payload) {
       state.collections = payload;
+      localStorage.setItem("collections", JSON.stringify(state.collections));
     },
     setHistory(state, payload) {
       state.history = payload;
+      localStorage.setItem("history", JSON.stringify(state.history));
     },
     historyPush(state, payload) {
       state.history.push(payload);
+      localStorage.setItem("collections", JSON.stringify(state.history));
     },
     historyPop(state) {
       state.history.pop();
+      localStorage.setItem("collections", JSON.stringify(state.history));
     },
     setIndex(state, payload) {
       state.index = payload;
+      localStorage.setItem("index", JSON.stringify(state.index));
     },
     increaseIndex(state) {
       let index = state.history.length - 1;
       if (state.index < state.history[index].content.length - 1) {
         state.index++;
       }
+      localStorage.setItem("index", JSON.stringify(state.index));
     },
     decreaseIndex(state) {
       if (state.index >= 1) {
         state.index--;
       }
+      localStorage.setItem("index", JSON.stringify(state.index));
     },
     setReport(state, payload) {
-      localStorage.setItem("report", payload);
       state.report = payload;
+      localStorage.setItem("report", state.report);
     }
   },
   actions: {},
