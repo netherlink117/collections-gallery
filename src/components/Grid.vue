ca2f2bdd1370fdce67e6453a48b96ff82f74c4f5
Daniel Gutierrez <netherlink117@outlook.com>
2021-09-25 20:56:03 -0700

    Update structure and style
    Change template structure and styels, also set up component to match store

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,31 +1,37 @@
 <template>
-  <div>
-    <div>
-      <div v-if="content.length === 0">
-        There is no content to show, try updating settings and then reload.
+  <div class="empty" v-if="directories.length === 0 && files.length === 0">
+    There is no content to show, try updating settings and then reload.
+  </div>
+  <div v-else class="grid">
+    <router-link
+      class="directory"
+      v-for="item of directories"
+      v-bind:key="item.path"
+      :to="{ name: 'Explorer', query: { path: item.path.replace('\\', '/') } }"
+    >
+      <div>
+        <i class="fas fa-folder fa-7x"></i>
       </div>
-      <div v-for="(item, index) of content" v-bind:key="index">
-        <div v-if="item.type === 'directory'">
-          <div>
-            <i class="fas fa-folder fa-7x"></i>
-          </div>
-          <div>
-            {{ item.name }}
-          </div>
-        </div>
-        <div v-else>
-          <div>
-            <i class="fas fa-file fa-5x"></i>
-          </div>
-          <div>
-            {{ item.name }}
-          </div>
-          <div>
-            {{ toHumanSize(item.size) }}
-          </div>
-        </div>
+      <div class="data">
+        {{ item.name }}
       </div>
-    </div>
+    </router-link>
+    <router-link
+      class="file"
+      v-for="item of files"
+      v-bind:key="item.path"
+      :to="{ name: 'Explorer', query: { path: item.path.replace('\\', '/') } }"
+    >
+      <div>
+        <i class="fas fa-file fa-7x"></i>
+      </div>
+      <div class="data">
+        {{ item.name }}
+      </div>
+      <div>
+        {{ toHumanSize(item.size) }}
+      </div>
+    </router-link>
   </div>
 </template>
 <script>
@@ -36,13 +42,18 @@ export default {
       return this.$store.state.order;
     },
     directories() {
-      return this.$store.state.collections.directories.items;
+      return this.$store.state.collections.directories.items.filter((item) => {
+        return (
+          item.parent.replace("\\", "/") === (this.$route.query.path || "/")
+        );
+      });
     },
     files() {
-      return this.$store.state.collections.files.items;
-    },
-    content() {
-      return [...this.directories, ...this.files];
+      return this.$store.state.collections.files.items.filter((item) => {
+        return (
+          item.parent.replace("\\", "/") === (this.$route.query.path || "/")
+        );
+      });
     }
   },
   methods: {
@@ -54,3 +65,37 @@ export default {
   }
 };
 </script>
+
+<style lang="sass">
+.grid
+  padding: 0.5rem
+  display: flex
+  flex-wrap: wrap
+  justify-content: center
+  gap: 1rem
+  > a
+    width: calc(50% - 1rem)
+    @media screen and (min-width:854px)
+      width: calc(25% - 1rem)
+    @media screen and (min-width:1280px)
+      width: calc(16.6% - 1rem)
+    @media screen and (min-width:1600px)
+      width: calc(12.5% - 1rem)
+    @media screen and (min-width:2560px)
+      width: calc(10% - 1rem)
+    padding: 0 1rem 0 1rem
+    height: 10rem
+    > div
+      width: 100%
+      text-align: center
+      text-overflow: ellipsis
+      white-space: nowrap
+      word-break: break-all
+      overflow: hidden
+  .directory
+    i
+      color: rgba(250,225,150, 1)
+  .file
+    i
+      color: rgba(220,220,220, 1)
+</style>
