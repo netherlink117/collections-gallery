f734c37564adfc48e2e5e14b20d6d7817d0e3c9f
Daniel Gutierrez <netherlink117@outlook.com>
2021-09-27 01:18:52 -0700

    Remove navigation fetch
    Removes fetching data before navigation

--- src/views/Explorer.vue
+++ src/views/Explorer.vue
@@ -12,23 +12,6 @@ export default {
     view() {
       return this.$store.state.view;
     }
-  },
-  methods: {
-    directories(to) {
-      return this.$store.state.collections.directories.items.filter((item) => {
-        return item.parent.replace("\\", "/") === (to.query.path || "/");
-      });
-    },
-    files(to) {
-      return this.$store.state.collections.files.items.filter((item) => {
-        return item.parent.replace("\\", "/") === (to.query.path || "/");
-      });
-    }
-  },
-  async beforeRouteUpdate(to) {
-    if (this.directories(to).length <= 0 && this.files(to).length <= 0) {
-      this.$store.dispatch("remotePath", to.query.path);
-    }
   }
 };
 </script>
