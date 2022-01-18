95bdf086096de1b342fcaa4d73570211f99fc8f3
Daniel Gutierrez <netherlink117@outlook.com>
2022-01-17 16:22:12 -0800

    Update index.php
    Fixes the

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ $is_collections_container = true;
 $is_ffmpeg_installed = true;
 $pathSeparator = PHP_OS_FAMILY === 'Windows' ? '\\' : '/';
 $rootPath = dirname(__FILE__);
-if ($is_collections_container) {
-  $rootPath = "/collections";
-}
 $rootPath = explode($pathSeparator, $rootPath);
 array_pop($rootPath);
 array_pop($rootPath);
 $rootPath = join($pathSeparator, $rootPath);
+if ($is_collections_container) {
+  $rootPath = "/collections";
+}
 if (isset($_GET['path'])) {
   try {
     $dir = str_replace('\\', $pathSeparator, $_GET['path']);
