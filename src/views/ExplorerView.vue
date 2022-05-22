b790c1ea506c1f4d4f960ba8feb631200145b7ff
Daniel Gutierrez <netherlink117dev@outlook.com>
2022-05-22 04:00:49 -0700

    Turn method into computed

--- src/views/ExplorerView.vue
+++ src/views/ExplorerView.vue
@@ -2,6 +2,7 @@
 import GridComponent from "@/components/GridComponent.vue";
 import { Directory } from "@/classes/Directory";
 import { useIndexStore } from "@/stores";
+import { computed } from "vue";
 
 const indexStore = useIndexStore();
 const props = defineProps({
@@ -11,18 +12,17 @@ const props = defineProps({
   }
 });
 
-// "p" is the last path used by vue-router, this ios because it seems like it has alot of recursive calls
-let d = "";
-
-// this functions creates the directory and uses indexStore to populate it, and asinnate to the state
-function getDirectoryFromProps(): Directory {
-  if (props.directory !== d) {
-    const directory = new Directory(props.directory, props.directory);
-    indexStore.getContent(directory);
-    d = props.directory;
+let loaded = "";
+const current = computed<Directory>(() => {
+  if (props.directory === loaded) return indexStore.current;
+  if (props.directory !== indexStore.current.path) {
+    indexStore.getContent(new Directory(props.directory.split("/").pop(), props.directory));
+  } else {
+    indexStore.getContent(indexStore.current);
   }
+  loaded = indexStore.current.path;
   return indexStore.current;
-}
+})
 
 function loadMore(): void {
   indexStore.getContent(indexStore.current);
@@ -30,11 +30,10 @@ function loadMore(): void {
 </script>
 
 <template>
-  <GridComponent :directory="getDirectoryFromProps()"></GridComponent>
+  <GridComponent :directory="current"></GridComponent>
   <button
     class="fixed bottom-7 right-7 mr-7 w-100px p-3 bg-emerald-500 hover:bg-emerald-700 rounded-4xl shadow text-center"
-    v-on:click="loadMore()"
-  >
+    v-on:click="loadMore">
     Load more
   </button>
 </template>
