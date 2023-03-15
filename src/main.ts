1e8de8585429169c7ecd8dfca5327091e98fada2
Daniel Gutierrez <netherlink117dev@outlook.com>
2023-03-15 02:30:14 -0700

    Add touch event stuff

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import App from "./App.vue";
 import router from "./router";
 import "virtual:windi.css";
 
+import Vue3TouchEvents from "vue3-touch-events";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(Vue3TouchEvents);
 
 app.mount("#app");
