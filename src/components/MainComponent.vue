e55809ba2bb1f489356a4f9b697f1736f935dc19
Daniel Gutierrez <netherlink117dev@outlook.com>
2023-08-13 01:56:38 -0700

    Add main header with navigator helper

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MainHeader from '@/components/MainHeaderComponent.vue'
+</script>
 
 <template>
   <main class="flex-1" style="overflow-y: scroll">
+    <MainHeader></MainHeader>
     <router-view></router-view>
   </main>
 </template>
